5ed11a4478e230579697e72a64069f2259567e96
Seekeye <166416561+Seekeye@users.noreply.github.com>
2025-10-03 21:21:32 +0200

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,21 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/health':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.end_headers()
+    
+    def log_message(self, format, *args):
+        pass
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8000))
+    server = HTTPServer(('', port), Handler)
+    server.serve_forever()
